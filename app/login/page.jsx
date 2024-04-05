c9da4d646cb10d7d14be7b247c7ed8af39da74d2
Halil-Ibrahim-Arslan <111198989+Halil-Ibrahim-Arslan@users.noreply.github.com>
2024-04-06 02:23:05 +0300

    Mongo db adımına gelindi.

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,12 @@
+import styles from "@/app/ui/login/login.module.css";
+import LoginForm from "../ui/login/loginForm/loginForm";
+
 const LoginPage = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
-}
+  return (
+    <div className={styles.container}>
+      <LoginForm/>
+    </div>
+  );
+};
 
 export default LoginPage;
