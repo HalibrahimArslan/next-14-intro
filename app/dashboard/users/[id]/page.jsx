2e95428d02f9c8f7e3baf0102b6ec36d60a1a4f2
halilibrahim.arslan <halilibrahim.arslan@aurorabilisim.com>
2024-07-08 11:55:24 +0300

    Next auth eklendi.

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -1,10 +1,10 @@
+import { fetchUser } from "@/app/lib/data";
 import styles from "@/app/ui/dashboard/users/singleUser/singleUser.module.css";
 import Image from "next/image";
 
 const SingleUserPage = async ({ params }) => {
-  
   const { id } = params;
-  //const user = await fetchUser(id);
+  const user = await fetchUser(id);
 
   return (
     <div className={styles.container}>
@@ -15,8 +15,8 @@ const SingleUserPage = async ({ params }) => {
         {user.username}
       </div>
       <div className={styles.formContainer}>
-        <form  className={styles.form}>
-          <input type="hidden" name="id" value={user.id}/>
+        <form className={styles.form}>
+          <input type="hidden" name="id" value={user.id} />
           <label>Username</label>
           <input type="text" name="username" placeholder={user.username} />
           <label>Email</label>
@@ -29,13 +29,21 @@ const SingleUserPage = async ({ params }) => {
           <textarea type="text" name="address" placeholder={user.address} />
           <label>Is Admin?</label>
           <select name="isAdmin" id="isAdmin">
-            <option value={true} selected={user.isAdmin}>Yes</option>
-            <option value={false} selected={!user.isAdmin}>No</option>
+            <option value={true} selected={user.isAdmin}>
+              Yes
+            </option>
+            <option value={false} selected={!user.isAdmin}>
+              No
+            </option>
           </select>
           <label>Is Active?</label>
           <select name="isActive" id="isActive">
-            <option value={true} selected={user.isActive}>Yes</option>
-            <option value={false} selected={!user.isActive}>No</option>
+            <option value={true} selected={user.isActive}>
+              Yes
+            </option>
+            <option value={false} selected={!user.isActive}>
+              No
+            </option>
           </select>
           <button>Update</button>
         </form>
--- app/lib/data.js
+++ app/lib/data.js
@@ -1,12 +1,39 @@
 import { User } from "./models";
 import { connectToDB } from "./utils";
 
+export const fetchUser = async (id) => {
+  console.log(id);
+  try {
+    connectToDB();
+    const user = await User.findById(id);
+    return user;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch user!");
+  }
+};
+
 export const fetchUsers = async (q) => {
   const regex = new RegExp(q, "i");
   try {
     await connectToDB();
     const users = await User.find({username:{$regex: regex}});
     return users;
+  } catch (error) {
+    console.log(error);
+    return { error };
+  }
+};
+
+export const fetchProducts = async (q, page) => {
+  const regex = new RegExp(q, "i");
+  try {
+    await connectToDB();
+    const count = await Product.countDocuments({title:{$regex: regex}});
+    const products = await Product.find({title:{$regex: regex}})
+      .skip((page - 1) * 10)
+      .limit(10);
+    return { count, products };
   } catch (error) {
     console.log(error);
     return { error };
